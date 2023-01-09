c85d45f388f4be6174ce25137756240f43803ae4
David Bastings <dbasting@f1r2s14.codam.nl>
2023-01-09 12:01:11 +0100

    Revert "Made libftprintf.h an alias of ft_printf.h."
    lifbtprint.h can't be an alias of ft_printf.h without causing norminette
    errors. (This reverts commit 36e258021d85908438a9aa765bb5f255c3230d2e.)

--- headers/libftprintf.h
+++ headers/libftprintf.h
@@ -1,1 +1,18 @@
-ft_printf.h
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   libftprintf.h                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dbasting <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/25 13:23:18 by dbasting      #+#    #+#                 */
+/*   Updated: 2022/11/28 15:15:15 by dbasting      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include "ft_printf.h"
+
+#endif
