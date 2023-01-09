b95905f5eab90fcb6f84f36222dd7eada1975ae5
David Bastings <dbasting@f1r2s14.codam.nl>
2023-01-09 12:28:00 +0100

    Got rid of ft_printf.h header: it just made things needlessly complicated.

--- headers/libftprintf.h
+++ headers/libftprintf.h
@@ -6,13 +6,19 @@
 /*   By: dbasting <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/25 13:23:18 by dbasting      #+#    #+#                 */
-/*   Updated: 2022/11/28 15:15:15 by dbasting      ########   odam.nl         */
+/*   Updated: 2023/01/09 12:23:37 by dbasting      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
 
-# include "ft_printf.h"
+# include <stdarg.h>
+# include <stddef.h>
+
+int	ft_printf(char const *format, ...);
+int	ft_dprintf(int fd, char const *format, ...);
+int	ft_vprintf(char const *format, va_list ap);
+int	ft_vdprintf(int fd, char const *format, va_list ap);
 
 #endif
